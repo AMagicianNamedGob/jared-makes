291077c2f28d824e6902f197a27c15a23a2cc34e
Jared Truscott <jtruscott78@gmail.com>
2023-02-17 16:01:01 -0500

    transferred old pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
-
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
@@ -17,7 +16,14 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx(), prefetch(), sitemap(), react(), tailwind()]
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    prefetch(),
+    sitemap(),
+    react(),
+    tailwind(),
+  ],
 });
