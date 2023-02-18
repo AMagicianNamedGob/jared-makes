954abb3b9f56222a1a543e75339128180f12a48f
Jared Truscott <jtruscott78@gmail.com>
2023-02-18 14:58:07 -0500

    debug
    tweak title
    
    debug astro image
    
    s

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../.astro/types.d.ts" />
 // / <reference types="astro/client" />
 // Replace `astro/client` with `@astrojs/image/client`
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
